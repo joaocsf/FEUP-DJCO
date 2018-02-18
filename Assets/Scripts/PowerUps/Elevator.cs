1e6d74a2b53031df3cefd6e948681d4fc478985b
Inês Proença <inesfproenca@gmail.com>
2018-02-18 17:57:16 +0000

    PowerUp System started

---
+++ Assets/Scripts/PowerUps/Elevator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : PowerUp {
+    // Use this for initialization
+    void Start () {
+        immediateUpdate = true;
+	}
+
+    // Update is called once per frame
+    void Update () {}
+
+    public override void Activate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/PowerUps/Trapdoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapdoor : PowerUp {
+
+    // Use this for initialization
+    void Start () {
+        immediateUpdate = false;
+    }
+
+    public override void Activate()
+    {
+        
+    }
+}
