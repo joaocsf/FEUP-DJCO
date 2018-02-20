224044e69658141eb70652ca394fe9fd9adc3314
noobcsf <noobcsf@hotmail.com>
2018-02-20 22:41:55 +0000

    Added Camera Transitions

---
+++ Assets/Scripts/Camera/CameraFX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFX : MonoBehaviour{
+
+	public Material mat;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest){
+		Graphics.Blit(src, dest, mat);
+	}
+}
