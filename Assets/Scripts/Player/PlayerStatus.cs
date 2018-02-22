ba1e55dccc31bd95da87b49b423ccfd169bf66a2
Lázaro Costa <lazaroosta@hotmail.com>
2018-02-22 10:20:16 +0000

    time

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -8,6 +8,12 @@ public class PlayerStatus : MonoBehaviour {
     private int playerId = 1;
     private int currentFloor = 0;
     public PlayerUIController controller;
+    public Text timerText;
+    private float secondsCount;
+    private int minuteCount;
+    private int hourCount;
+
+
 
     private GameController gameController;
 
@@ -17,6 +23,10 @@ public class PlayerStatus : MonoBehaviour {
         gameController = GameObject.FindObjectOfType<GameController>();
         yield return new WaitForFixedUpdate();
         controller = UIManager.instance.createPlayerUI();
+        secondsCount = 0;
+        minuteCount = 0;
+        hourCount = 0;
+        UpdateTimerUI();
     }
 
 	
@@ -31,11 +41,33 @@ public class PlayerStatus : MonoBehaviour {
             currentFloor = newFloor;
             UpdateText();
         }
+
+        UpdateTimerUI();
+
     }
 
 
     private void UpdateText()
     {
         controller.SetScoreText("<b>Player " + playerId + "</b>\n" + currentFloor + "F");
+    }
+
+
+
+    private void UpdateTimerUI()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        timerText.text = hourCount + "h:" + minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
     }
 }
