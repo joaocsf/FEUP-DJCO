310936ba85bb60d0be12d7e371c99a8872d31f3f
lazarocosta <lazaroosta@hotmail.com>
2018-03-01 23:23:56 +0000

    add files

---
+++ Assets/Scripts/Entity/Elevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartialElevator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+		
+			//Destroy(other.gameObject);
+
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
