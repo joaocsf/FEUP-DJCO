d14a3414a3af61f277fa41f7e1b6e7fb607e2788
lazarocosta <lazaroosta@hotmail.com>
2018-03-02 00:36:10 +0000

    elevator initialized

--- Assets/Scripts/Entity/PartialElevator.cs
+++ Assets/Scripts/Entity/PartialElevator.cs
@@ -4,24 +4,15 @@ using UnityEngine;
 
 public class PartialElevator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.CompareTag("Player"))
 		{
-		
-			//Destroy(other.gameObject);
-
-
+			GameObject player = other.gameObject;
+			Vector3 velocida= new Vector3(0, 4, 0);
+			player.transform.Translate(velocida);
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
